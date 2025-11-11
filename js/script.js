46eaeb6491c06270295182c0a4fe098d40ff498f
Jay Owings <jaydenowings@mail.weber.edu>
2025-11-10 18:38:20 -0700

    Video 4

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function init () {
     switch(global.currentPage){
         case '/':
         case '/index.html':
-            console.log('Home');
+            displayPopularMovie();
             break;
         case '/shows.html':
             console.log('Shows');
@@ -33,4 +33,23 @@ function highlightActiveLink() {
             link.classList.add('active')
         }
     }))
+}
+
+async function fetchAPIData(endpoint) {
+    const API_KEY = '974f65d139f462cf50d6e015bca6ae0e'
+    const API_URL = 'https://api.themoviedb.org/3/'
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`)
+    const data = await response.json()
+    return data
+}
+
+async function displayPopularMovie() {
+    const { results} = await fetchAPIData('movie/popular')
+    results.forEach((movie) => {
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML = `<a href = "movie-details.html?id=${movie.id}"> ${movie.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class = "card-img-top" alt = ${movie.title}"/>` :`<img scr="../images/no-image.jpg" class = "card-img-top" alt = "${movie.title}"/>`} </a> <div class="card-body"> <h5 class="card-title">${movie.title}</h5> <p class="card-text"> <small class="text-muted">Release: ${movie.release_date}</small> </p> </div>`
+        document.querySelector('#popular-movies').appendChild(div)
+    })
+    
 }
