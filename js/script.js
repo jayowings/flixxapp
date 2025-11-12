c62e5094498541861f7b15bfe2485a169203ea55
Jay Owings <jaydenowings@mail.weber.edu>
2025-11-12 14:53:39 -0700

    Video 5

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function init () {
             displayPopularMovie();
             break;
         case '/shows.html':
-            console.log('Shows');
+            displayPopularShows()
             break;
         case '/movie-details.html':
             console.log('Movie Details');
@@ -38,8 +38,12 @@ function highlightActiveLink() {
 async function fetchAPIData(endpoint) {
     const API_KEY = '974f65d139f462cf50d6e015bca6ae0e'
     const API_URL = 'https://api.themoviedb.org/3/'
+
+    showSpinner()
     const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`)
     const data = await response.json()
+
+    hideSpinner()
     return data
 }
 
@@ -51,5 +55,22 @@ async function displayPopularMovie() {
         div.innerHTML = `<a href = "movie-details.html?id=${movie.id}"> ${movie.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class = "card-img-top" alt = ${movie.title}"/>` :`<img scr="../images/no-image.jpg" class = "card-img-top" alt = "${movie.title}"/>`} </a> <div class="card-body"> <h5 class="card-title">${movie.title}</h5> <p class="card-text"> <small class="text-muted">Release: ${movie.release_date}</small> </p> </div>`
         document.querySelector('#popular-movies').appendChild(div)
     })
-    
+}
+
+async function displayPopularShows() {
+    const { results} = await fetchAPIData('tv/popular')
+    results.forEach((show) => {
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML = `<a href = "tv-details.html?id=${show.id}"> ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class = "card-img-top" alt = ${show.name}"/>` :`<img scr="../images/no-image.jpg" class = "card-img-top" alt = "${show.name}"/>`} </a> <div class="card-body"> <h5 class="card-title">${show.name}</h5> <p class="card-text"> <small class="text-muted">First Air Date: ${show.first_air_date}</small> </p> </div>`
+        document.querySelector('#popular-shows').appendChild(div)
+    })
+}
+
+function showSpinner(){
+    document.querySelector('.spinner').classList.add('show')
+}
+
+function hideSpinner(){
+    document.querySelector('.spinner').classList.remove('show')
 }
